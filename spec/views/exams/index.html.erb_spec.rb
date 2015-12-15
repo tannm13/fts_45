368cfb3383660f352417394ceba2c7cc38ce104d
tannm13 <tannm132@gmail.com>
2015-12-15 09:40:30 +0700

    fix-some-view-rspecs

--- spec/views/exams/index.html.erb_spec.rb
+++ spec/views/exams/index.html.erb_spec.rb
@@ -1,21 +1,26 @@
 require "rails_helper"
 
 describe "exams/index.html.erb" do
-  subject {rendered}
   let(:user) {create :user}
   let(:subject) {create :subject}
-  let(:exam) {create :exam}
+  let(:exam) {create :exam, subject: subject, user: user}
 
   before do
     sign_in user
     assign :exam, exam
-    assign :exams, exam
-    assign :subjects, subject
+    assign :exams, user.exams.paginate(page: 1)
+    assign :subjects, [subject]
     render
   end
 
   it do
-    is_expected.to have_selector "form", method: "post", action: exam_path do |form|
+    expect(controller.request.path_parameters[:controller]).to eq("exams")
+    expect(controller.request.path_parameters[:action]).to eq("index")
+    expect(controller.controller_path).to eq("exams")
+  end
+
+  it do
+    expect(rendered).to have_selector "form" do |form|
       form.is_expected.to have_selector "input", name: "user_id"
       form.is_expected.to have_selector "input", name: "status"
       form.is_expected.to have_selector "input", name: "duration"
@@ -26,12 +31,11 @@ describe "exams/index.html.erb" do
   end
 
   it do
-    is_expected.to have_content exam.created_at
-    is_expected.to have_content exam.subject.name
-    is_expected.to have_content exam.status
-    is_expected.to have_content exam.duration
-    is_expected.to have_content exam.question_number
-    is_expected.to have_content exam.spent_time
-    is_expected.to have_content exam.score
+    expect(rendered).to have_content exam.subject.name
+    expect(rendered).to have_content exam.status
+    expect(rendered).to have_content exam.duration
+    expect(rendered).to have_content exam.question_number
+    expect(rendered).to have_content exam.spent_time
+    expect(rendered).to have_content exam.score
   end
 end
